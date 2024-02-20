23810b04fe1aa641bd625baba7fbd1964cb87716
gnanamoorthy131 <gnanamoorthy@eagleteleservices.biz>
2024-02-20 16:21:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
+
+//comment
+
 function App() {
+  const NavigationBar =['Home','Services','About','Skills','Portfolio','Contact']
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Logo">
+        <h2>GM</h2>
+      </div>
+      <div className="Navigation">
+       {NavigationBar.map(nav=> <p className='Nav'>{nav}</p>)}
+      </div>
     </div>
+    <div className='Content' >
+    <p className='Major'>Hello, I'm Gnana Moorthy<br></br>a Back end Developer</p>
+    <h2 className='Minor'>I'm help to build and maintain the mechanisms<br></br> that process data and perform actions on websites</h2>
+    <button className='Talk'>Let's Talk</button>
+</div>
+    </>
   );
 }
 
